5b7a19773870c63d9e4ca9cc8afe09b551f367fa
bartimar6@gmail.com <marek.bartik@ackee.cz>
2016-12-09 17:34:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Backup container for PostgreSQL instances to Google Storage
+
+This image provides a cron daemon that runs daily backups from postgres (clustered or single instance) to Google Storage.
+
+Following ENV variables must be specified:
+ - `POSTGRES_HOST` contains the remote host (hostname or IP) connection string for pg_dump command line client option -h
+ - `POSTGRES_PORT` contains the remote port number for pg_dump option -P
+  - `postgresserver.domain.com:5432` in case of a single instance
+ - `POSTGRES_DB` database name 
+ - `POSTGRES_USER` username used for connecting to the database
+ - `POSTGRES_PASSWORD` password of user `POSTGRES_USER` who has access to all dbs
+ - `GS_URL` contains address in GS where to store backups, without the `gs://` prefix
+  - `bucket-name/directory`
+ - `GS_ACCESS_KEY`
+ - `GS_SECRET_KEY`
+ - `CRON_SCHEDULE` cron schedule string, default '0 2 * * *'
+
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eo pipefail
+
+backup_tool="gsutil"
+
+# verify variables
+if [ -z "$GS_ACCESS_KEY" -o -z "$GS_SECRET_KEY" -o -z "$GS_URL" -o -z "$POSTGRES_HOST" -o -z "$POSTGRES_PORT" -o -z "$POSTGRES_DB" ]; then
+	echo >&2 'Backup information is not complete. You need to specify GS_ACCESS_KEY, GS_SECRET_KEY, GS_URL, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB. No backups, no fun.'
+	exit 1
+fi
+
+# set gs config
+echo -e "[Credentials]\ngs_access_key_id = $GS_ACCESS_KEY\ngs_secret_access_key = $GS_SECRET_KEY" > /root/.boto
+
+# verify GS config
+$backup_tool ls "gs://$GS_URL" > /dev/null
+
+# set cron schedule TODO: check if the string is valid (five or six values separated by white space)
+[[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
+   echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
+
+# format hostname:port:database:username:password 
+# for more information see https://www.postgresql.org/docs/9.1/static/libpq-pgpass.html
+echo "*:*:*:*:$POSTGRES_PASSWORD" > ~/.pgpass
+chmod 0600 ~/.pgpass
+
+DB_URI="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+# add a cron job
+echo "$CRON_SCHEDULE root rm -rf /tmp/dump* && pg_dumpall --dbname=$DB_URI --file=/tmp/dump.sql --verbose >> /var/log/cron.log 2>&1 && gzip -c /tmp/dump.sql > /tmp/dump && $backup_tool mv /tmp/dump gs://$GS_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump*" >> /etc/crontab
+crontab /etc/crontab
+
+exec "$@"
