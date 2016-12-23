0737b513d3bfc2b41c922a12abc84ce31d93be70
Marek Bartik <bartimar@users.noreply.github.com>
2016-12-23 16:57:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ fi
 # set gs config
 echo -e "[Credentials]\ngs_access_key_id = $GS_ACCESS_KEY\ngs_secret_access_key = $GS_SECRET_KEY" > /root/.boto
 
-# verify GS config
-$backup_tool ls "gs://$GS_URL" > /dev/null
+# verify gs config - ls bucket
+$backup_tool ls "gs://${GS_URL%%/*}" > /dev/null
 
 # set cron schedule TODO: check if the string is valid (five or six values separated by white space)
 [[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
@@ -27,7 +27,7 @@ chmod 0600 ~/.pgpass
 
 DB_URI="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
 # add a cron job
-echo "$CRON_SCHEDULE root rm -rf /tmp/dump* && pg_dumpall --dbname=$DB_URI --file=/tmp/dump.sql --verbose >> /var/log/cron.log 2>&1 && gzip -c /tmp/dump.sql > /tmp/dump && $backup_tool $backup_options /tmp/dump gs://$GS_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump*" >> /etc/crontab
+echo "$CRON_SCHEDULE root  mkdir -p /tmp/backup ; rm -rf /tmp/backup/* && pg_dumpall --dbname=$DB_URI --file=/tmp/backup/dump.sql --verbose >> /var/log/cron.log 2>&1 && gzip -c /tmp/backup/dump.sql > /tmp/backup/dump.gz && $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/backup/*" >> /etc/crontab
 crontab /etc/crontab
 
 exec "$@"
